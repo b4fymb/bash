7dffebcdb5074ed404e796718943e31230271862
b4fymb <34404726+b4fymb@users.noreply.github.com>
2024-01-29 15:36:29 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -357,7 +357,7 @@ nginx_install() {
 
     wget -nc --no-check-certificate http://nginx.org/download/nginx-${nginx_version}.tar.gz -P ${nginx_openssl_src}
     judge "Nginx 下载"
-    wget -nc --no-check-certificate https://artfiles.org/openssl.org/source/openssl-1.1.1w.tar.gz -P ${nginx_openssl_src}
+    wget -nc --no-check-certificate https://artfiles.org/openssl.org/source/old/1.1.1/openssl-1.1.1k.tar.gz -P ${nginx_openssl_src}
     judge "openssl 下载"
     wget -nc --no-check-certificate https://github.com/jemalloc/jemalloc/releases/download/${jemalloc_version}/jemalloc-${jemalloc_version}.tar.bz2 -P ${nginx_openssl_src}
     judge "jemalloc 下载"
